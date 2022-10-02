162a5b1e2dd11e827abc01128d952c9838cca6c0
Tevin-creator <isaactevin67@gmail..com>
2022-10-02 17:34:40 +0300

    updated default app

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,130 @@
+import React from 'react';
+import { MDBFooter } from 'mdb-react-ui-kit';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faYoutube,
+  faFacebook,
+  faTwitter,
+  faInstagram
+} from "@fortawesome/free-brands-svg-icons";
+
+export default function App() {
+  return (
+    <MDBFooter bgColor='light' className='text-center text-lg-start text-muted'>
+      <section className='d-flex justify-content-center justify-content-lg-between p-4 border-bottom'>
+        <div className='me-5 d-none d-lg-block'>
+          <span>Get connected with us on social networks:</span>
+        </div>
+
+        <a
+          href="https://www.youtube.com/"
+          className="youtube social"
+        >
+          <FontAwesomeIcon icon={faYoutube} size="2x" />
+        </a>
+        <a
+          href="https://www.facebook.com/"
+          className="facebook social"
+        >
+          <FontAwesomeIcon icon={faFacebook} size="2x" />
+        </a>
+        <a
+          href="http://www.instagram.com"
+          className="instagram social"
+        >
+          <FontAwesomeIcon icon={faInstagram} size="2x" />
+        </a>
+        <a href="https://wwww.twitter.com" className="twitter social">
+          <FontAwesomeIcon icon={faTwitter} size="2x" />
+        </a>
+      </section>
+
+      <section className=''>
+        <div className='container text-center text-md-start mt-5'>
+          <div className='row mt-3'>
+            <div className='col-md-3 col-lg-4 col-xl-3 mx-auto mb-4'>
+              <h6 className='text-uppercase fw-bold mb-4'>
+                <i className='fas fa-gem me-3'></i>Booksville
+              </h6>
+              <p>
+              Reading a real book helps you relax more than zoning out in front of a screen before bed. Screens like e-readers and tablets can actually keep you awake longer and even hurt your sleep. That applies to kids too: Fifty-four percent of children sleep near a small screen, and clock 20 fewer minutes of shut-eye on average because of it.
+              </p>
+            </div>
+
+            <div className='col-md-2 col-lg-2 col-xl-2 mx-auto mb-4'>
+              <h6 className='text-uppercase fw-bold mb-4'>Featured Books</h6>
+              <p>
+                <a href='#!' className='text-reset'>
+                 Reality
+                </a>
+              </p>
+              <p>
+                <a href='#!' className='text-reset'>
+                  Fantasy
+                </a>
+              </p>
+              <p>
+                <a href='#!' className='text-reset'>
+                  Adventure
+                </a>
+              </p>
+              <p>
+                <a href='#!' className='text-reset'>
+                  Science
+                </a>
+              </p>
+            </div>
+
+            <div className='col-md-3 col-lg-2 col-xl-2 mx-auto mb-4'>
+              <h6 className='text-uppercase fw-bold mb-4'>Useful links</h6>
+              <p>
+                <a href='#!' className='text-reset'>
+                  Pricing
+                </a>
+              </p>
+              <p>
+                <a href='#!' className='text-reset'>
+                  Settings
+                </a>
+              </p>
+              <p>
+                <a href='#!' className='text-reset'>
+                  Orders
+                </a>
+              </p>
+              <p>
+                <a href='#!' className='text-reset'>
+                  Help
+                </a>
+              </p>
+            </div>
+
+            <div className='col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4'>
+              <h6 className='text-uppercase fw-bold mb-4'>Contact</h6>
+              <p>
+                <i className='fas fa-home me-3'></i> Nairobi Kenya
+              </p>
+              <p>
+                <i className='fas fa-envelope me-3'></i>
+                isaactevin67@gmail.com
+              </p>
+              <p>
+                <i className='fas fa-phone me-3'></i> + 01 234 567 88
+              </p>
+              <p>
+                <i className='fas fa-print me-3'></i> + 245 700 000 00
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <div className='text-center p-4' style={{ backgroundColor: 'rgba(0, 0, 0, 0.05)' }}>
+        © 2022 Copyright:
+        <a className='text-reset fw-bold' href='https://www.healthline.com/health/benefits-of-reading-books#alleviates-depression/'>
+          World Bookstore
+        </a>
+      </div>
+    </MDBFooter>
+  );
+}
